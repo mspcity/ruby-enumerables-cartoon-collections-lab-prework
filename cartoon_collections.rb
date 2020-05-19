4a0cbd7b84e492ffbc917aa2ebe0026b135d6ac8
Youngmin Park <codepark2019@gmail.com>
2020-05-19 06:33:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(long_planeteer_calls)
-  long_planeteer_calls.map do |call|
+  long_planeteer_calls.each do |call|
     if call.length > 4
       return true
     end
